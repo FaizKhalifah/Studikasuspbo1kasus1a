80aa4e43e65ab1192630959c3cd8f38c76a84deb
FaizKhalifah <118319595+FaizKhalifah@users.noreply.github.com>
2023-03-07 23:21:51 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Karyawan karyawan1 = new Karyawan();
-        karyawan1.setNama("Yasin");
-        karyawan1.setAlamat("Duren Sawit");
-        karyawan1.setNoTelepon("0892828292");
-        karyawan1.setJenisKelamin("Laki laki");
-        karyawan1.setKategoriKaryawan("groomer");
-        karyawan1.setPendapatan(90);
-        karyawan1.cetakInfo();
-
-        Karyawan karyawan2 = new Karyawan("Ravi","Sleman","09898998","laki laki","dokter hewan", 100);
-        karyawan2.cetakInfo();
+          Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        input.nextLine();
+        Karyawan[] listkaryawan = new Karyawan[a];
+        for (int i =0;i<a;i++){
+            String nama= input.nextLine();
+            String alamat = input.nextLine();
+            String noTelepon = input.nextLine();
+            String jenisKelamin = input.nextLine();
+            String kategori = input.nextLine();
+            double pendapatan = input.nextDouble();
+            input.nextLine();
+            listkaryawan[i] = new Karyawan(nama,alamat,noTelepon,jenisKelamin,kategori,pendapatan);
+            listkaryawan[i].cetakInfo();
+        }
     }
 }
